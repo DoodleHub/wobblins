3214374ca13a8fedfa5f3ffa463eed79c1971e79
Chao Chen <chaochen@live.com>
2026-07-19 14:55:34 -0400

    chore: set up nativewind

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,6 +2,8 @@ import { Stack } from "expo-router";
 
 import { SupabaseProvider } from "@/supabase/SupabaseProvider";
 
+import "../../global.css";
+
 export default function RootLayout() {
   return (
     <SupabaseProvider>
